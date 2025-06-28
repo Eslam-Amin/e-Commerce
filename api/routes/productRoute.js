85efe21f2e0db56bb7985a7815a3a0efbaeceb93
Eslam Amin <eslam.amin.610@gmail.com>
2025-06-28 14:36:11 +0300

    Made Changes to the routes to apply Product Controller Instance

--- api/routes/productRoute.js
+++ api/routes/productRoute.js
@@ -1,25 +1,52 @@
-const express = require('express');
-const { getAllProducts, getProductDetails, updateProduct, deleteProduct, getProductReviews, deleteReview, createProductReview, createProduct, getAdminProducts, getProducts } = require('../controllers/productController');
-const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/user_actions/auth');
+const express = require("express");
+const productController = require("../controllers/productController");
+const {
+  isAuthenticatedUser,
+  authorizeRoles
+} = require("../middlewares/user_actions/auth");
 
 const router = express.Router();
 
-router.route('/products').get(getAllProducts);
-router.route('/products/all').get(getProducts);
+router.route("/products").get(productController.getAllProducts);
+router.route("/products/all").get(productController.getProducts);
 
-router.route('/admin/products').get(isAuthenticatedUser, authorizeRoles("admin"), getAdminProducts);
-router.route('/admin/product/new').post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
+router
+  .route("/admin/products")
+  .get(
+    isAuthenticatedUser,
+    authorizeRoles("admin"),
+    productController.getAdminProducts
+  );
+router
+  .route("/admin/product/new")
+  .post(
+    isAuthenticatedUser,
+    authorizeRoles("admin"),
+    productController.createProduct
+  );
 
-router.route('/admin/product/:id')
-    .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
-    .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
+router
+  .route("/admin/product/:id")
+  .put(
+    isAuthenticatedUser,
+    authorizeRoles("admin"),
+    productController.updateProduct
+  )
+  .delete(
+    isAuthenticatedUser,
+    authorizeRoles("admin"),
+    productController.deleteProduct
+  );
 
-router.route('/product/:id').get(getProductDetails);
+router.route("/product/:id").get(productController.getProductDetails);
 
-router.route('/review').put(isAuthenticatedUser, createProductReview);
+router
+  .route("/review")
+  .put(isAuthenticatedUser, productController.createProductReview);
 
-router.route('/admin/reviews')
-    .get(getProductReviews)
-    .delete(isAuthenticatedUser, deleteReview);
+router
+  .route("/admin/reviews")
+  .get(productController.getProductReviews)
+  .delete(isAuthenticatedUser, productController.deleteReview);
 
 module.exports = router;
